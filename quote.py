fde10f11dc2c553b5e6c6817075e0d3d8cf85f9f
aydynbaily <60446518+aydynbaily@users.noreply.github.com>
2020-02-07 13:52:09 -0600

    Create quote.py

---
+++ quote.py
@@ -0,0 +1,12 @@
+# Course: CS 30
+# Period: 4
+# Date created: 20/02/03
+# Date last modified: 20/02/03
+# Name: Aydyn Baily
+# Description: Prints a famous quote
+
+
+famous_person = "Stephen Hawking"
+quote = "Life would be tragic if it were not funny"
+message = f"{name.title()} once said {quote.lower()}"
+print (message)
